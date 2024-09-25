cdcf1b12a022cd3bb05aa28efd4172c8e6386f63
pandey192 <pandeyshivam192@gmail.com>
2024-09-25 11:54:02 +0530

    code push again

---
+++ code/basics/24sep_2024/Test_lab01.py
@@ -0,0 +1,31 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_open_login():
+    driver = webdriver.Chrome()
+    driver.get("https://katalon-demo-cura.herokuapp.com/")
+
+    make_appoitnment_btn = driver.find_element(By.ID,"btn-make-appointment")
+    make_appoitnment_btn.click()
+
+    print(driver.current_url) #verification of the url #assert
+
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
+
+    username_btn = driver.find_element(By.ID,"txt-username")
+    username_btn.send_keys("John Doe")
+    password_btn = driver.find_element(By.ID,"txt-password")
+    password_btn.send_keys("ThisIsNotAPassword")
+    submit_btn = driver.find_element(By.ID,"btn-login")
+    submit_btn.click()
+
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/#appointment","Error wrong Url"
+
+
+
+
+
+    time.sleep(5)
+    driver.quit()
