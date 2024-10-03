e2bc62779bea8c26aba453e6b44a8465ca4bd814
pandey192 <pandeyshivam192@gmail.com>
2024-10-03 15:02:03 +0530

    code push 30sep

---
+++ code/basics/24sep_2024/Test_lab05.py
@@ -0,0 +1,36 @@
+#Css Selector
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_open_login():
+    driver = webdriver.Chrome()
+    driver.get("https://katalon-demo-cura.herokuapp.com/")
+
+
+    make_appoitnment_btn = driver.find_element(By.CSS_SELECTOR,"a[id='btn-make-appointment']") #using Css Selector
+    make_appoitnment_btn.click()
+
+    list_elements_p = driver.find_elements(By.XPATH, "//p[contains(text(),'A')]")
+    for i in list_elements_p:
+        if i.text == "Copyright © CURA Healthcare Service 2024":
+            i.click()
+        print(i.text)
+
+    print(driver.current_url) #verification of the url #assert
+
+    #assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
+
+    username_btn = driver.find_element(By.ID,"txt-username")
+    username_btn.send_keys("John Doe")
+    password_btn = driver.find_element(By.ID,"txt-password")
+    password_btn.send_keys("ThisIsNotAPassword")
+    submit_btn = driver.find_element(By.ID,"btn-login")
+    submit_btn.click()
+
+
+    #assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/#appointment","Error wrong Url"
+
+
+    time.sleep(5)
+    driver.quit()
